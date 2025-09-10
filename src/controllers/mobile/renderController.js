5e4bacf8df7d9a2df943591bd5c979bf430126cb
kyrawest <kyrawest@hotmail.com>
2025-09-10 14:19:32 -0700

    Added JSON API endpoint for mobile dashboards
    /mobile/dashboard

---
+++ src/controllers/mobile/renderController.js
@@ -0,0 +1,34 @@
+import userHandler from "../../handlers/userHandler.js";
+import runHandler from "../../handlers/runHandler.js";
+import noteHandler from "../../handlers/noteHandler.js";
+import createHttpError from "http-errors";
+
+const dashboard = async (req, res) => {
+  //Renders dashboard page, passes run JSON data to the frontend
+  const username = req.user.username;
+  const user = req.user;
+
+  //dashboard has pagination: displays 10 runs at a time. By default it will open to page 1 unless specified in req.query
+  let pageNumber = 1;
+  if (req.query.page) {
+    pageNumber = req.query.page;
+  }
+
+  //get runs for the user - expects an array of run objects (max 10 objects).
+  const runs = await userHandler.getThisUserRuns(req.user._id, pageNumber);
+
+  // For each run in the runs array, also add the notes for that run.
+  // Use Promise.all to resolve all async operations in parallel - keeps the map function from failing out
+  await Promise.all(
+    runs.map(async (run) => {
+      run.notes = await runHandler.getRunNotes(run._id);
+    })
+  );
+
+  // Render the page with our gathered data.
+  return runs;
+};
+
+export default {
+  dashboard,
+};
--- src/routes/mobile/mobileRouter.js
+++ src/routes/mobile/mobileRouter.js
@@ -4,11 +4,21 @@ import userRouter from "./userRoutes.js";
 import runRouter from "./runRoutes.js";
 import noteRouter from "./noteRoutes.js";
 
+import renderController from "../../controllers/mobile/renderController.js";
+
+import { isLoggedIn } from "../../auth/auth.js";
+import { catchErrors } from "../../handlers/errorHandlers.js";
+
 const mobileRouter = Router();
 
 // Prefix each module router for clarity in the mobile API
 mobileRouter.use("/users", userRouter); // all user-related endpoints
 mobileRouter.use("/runs", runRouter); // all run-related endpoints
 mobileRouter.use("/notes", noteRouter); // all note-related endpoints
+mobileRouter.use(
+  "/dashboard",
+  isLoggedIn,
+  catchErrors(renderController.dashboard)
+); // Mobile dashboard rendering endpoint
 
 export default mobileRouter;
