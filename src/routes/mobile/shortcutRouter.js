923d7022dab2ece34c77c24f3efe1d5c46209e46
kyrawest <kyrawest@hotmail.com>
2025-10-19 12:24:02 -0700

    Fixed shortcut token

--- src/routes/mobile/shortcutRouter.js
+++ src/routes/mobile/shortcutRouter.js
@@ -36,7 +36,7 @@ shortcutRouter.post("/generate", verifyJWT, async (req, res) => {
       scope: payload.scope,
     });
 
-    console.log("Created Shortcut token:", newToken);
+    console.log("Created Shortcut token:", token);
     res.json({ token });
   } catch (err) {
     console.error("Error creating Shortcut token:", err);
